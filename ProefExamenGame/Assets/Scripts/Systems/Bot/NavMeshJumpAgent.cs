4553d709833ebef5b815a22c0d93f27857d67198
owen22s <34884@ma-web.nl>
2026-02-18 12:22:05 +0100

    Updated jumping script

--- ProefExamenGame/Assets/Scripts/Systems/Bot/NavMeshJumpAgent.cs
+++ ProefExamenGame/Assets/Scripts/Systems/Bot/NavMeshJumpAgent.cs
@@ -6,6 +6,7 @@ public class NavMeshJumpAgent : MonoBehaviour
 {
     private NavMeshAgent _agent;
     private Rigidbody _rb;
+    private bool _isJumping = false;
 
     public float jumpForce = 8f;
     public float jumpDuration = 0.5f;
@@ -18,7 +19,7 @@ public class NavMeshJumpAgent : MonoBehaviour
 
     void Update()
     {
-        if (_agent.isOnOffMeshLink)
+        if (_agent.isOnOffMeshLink && !_isJumping)
         {
             StartCoroutine(JumpAcross());
         }
@@ -26,25 +27,32 @@ public class NavMeshJumpAgent : MonoBehaviour
 
     IEnumerator JumpAcross()
     {
+        _isJumping = true;
         _agent.isStopped = true;
         _agent.updatePosition = false;
 
         OffMeshLinkData data = _agent.currentOffMeshLinkData;
+        Vector3 startPos = transform.position;
         Vector3 endPos = data.endPos + Vector3.up * 0.1f;
 
-        // jump physics
+        // Calculate velocity needed to reach endPos in jumpDuration
+        Vector3 velocity = (endPos - startPos) / jumpDuration;
+        velocity.y = jumpDuration * 0.5f * -Physics.gravity.y; // arc upward
+
         _rb.isKinematic = false;
-        _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        _rb.velocity = velocity;
 
         yield return new WaitForSeconds(jumpDuration);
 
-        // teleport to end of link
-        transform.position = endPos;
-
+        // Snap to end of link
+        _rb.velocity = Vector3.zero;
         _rb.isKinematic = true;
+        transform.position = endPos;
 
         _agent.CompleteOffMeshLink();
         _agent.updatePosition = true;
         _agent.isStopped = false;
+
+        _isJumping = false;
     }
 }
