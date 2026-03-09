5853931521118e25d46e97014bf9e49a9c88db5b
owen22s <34884@ma-web.nl>
2026-03-09 09:32:32 +0100

    updated summary

--- ProefExamenGame/Assets/Scripts/RespawnSystem/RespawnSystem.cs
+++ ProefExamenGame/Assets/Scripts/RespawnSystem/RespawnSystem.cs
@@ -2,12 +2,13 @@
 using UnityEngine.AI;
 using UnityEngine.Events;
 
-/// <summary>
-/// Handles killing volume logic and respawning players or bots.
-/// Works with AIMoveNavMesh bots for patrol and jump reset.
-/// </summary>
+
 public class RespawnSystem : MonoBehaviour
 {
+    /// <summary>
+    /// Handles killing volume logic and respawning players or bots.
+    /// Works with AIMoveNavMesh bots for patrol and jump reset.
+    /// </summary>
     public UnityEvent died;
 
     private void OnTriggerEnter(Collider other)
